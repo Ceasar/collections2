d351e30f58306f32d76e4f569849fb05bbc08a5c
Ceasar Bautista <cbautista2010@gmail.com>
2014-02-22 11:06:49 -0500

    split up abstract

---
+++ collections2/queue.py
@@ -0,0 +1,50 @@
+"""
+While a deque can do anything a queue can do and more, by limiting the number
+of available actions we achieve two things:
+
+- It becomes semantically clear how we intend to use a data-structure.
+
+- We can strengthen the invariants.
+
+A similar argument can be made for using a stack in place of a list.
+"""
+from collections import deque
+
+
+class Queue(object):
+    """
+    A first-in first-out data structure.
+
+    A queue is guaranteed to always have at least 0 elements, and to always
+    maintain the first-in first-out property.
+
+    """
+    def __init__(self):
+        self._items = deque()
+
+    @property
+    def top(self):
+        return self._items[0]
+
+    @property
+    def empty(self):
+        return len(self) == 0
+
+    def push(self, item):
+        '''Q.push(object) -- push object on back of queue.'''
+        self._items.append(item)
+
+    def pop(self):
+        '''Q.pop() -> item -- remove and return first item.
+        Raises IndexError if queue is empty.'''
+        try:
+            return self._items.popleft()
+        except IndexError:
+            raise IndexError("pop from empty queue")
+
+    def __len__(self):
+        return len(self._items)
+
+    def __iter__(self):
+        while not self.empty:
+            yield self.pop()
